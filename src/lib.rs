d2e1944054fa440911badd8db0dd85be45ba90d1
jenra <24357776+jenra-uwu@users.noreply.github.com>
2021-03-03 12:50:29 -0500

    Added tuple functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 use std::ops;
 
+// ==================================================================
+// Arithmetic
+// ==================================================================
+
+// id :: forall a . a -> a
 pub fn morloc_id<T>(t: T) -> T{
     t
 }
@@ -28,10 +33,50 @@ pub fn morloc_mod(a: i64, b: i64) -> i64 {
     a % b
 }
 
+// ==================================================================
+// Tuples
+// ==================================================================
+
+// forall a b . (a, b) -> a
+pub fn fst<T, U>(v: &(T, U)) -> &T {
+    &v.0
+}
+
+// forall a b . (a, b) -> a
+pub fn snd<T, U>(v: &(T, U)) -> &U {
+    &v.1
+}
+
+// forall a b . a -> b -> (a, b)
+pub fn tuple<T, U>(t: T, u: U) -> (T, U) {
+    (t, u)
+}
+
+// forall a b . (a -> b) -> a -> (b, a)
+pub fn couple<T, U>(f: fn(&T) -> U, t: T) -> (U, T) {
+    (f(&t), t)
+}
+
+// forall a b c . (b -> c) -> (a, b) -> (a, c)
+pub fn with_sec<T, U, V>(f: fn(&U) -> V, v: (T, U)) -> (T, V) {
+    (v.0, f(&v.1))
+}
+
+// forall a b c . (a -> c) -> (a, b) -> (c, b)
+pub fn with_fst<T, U, V>(f: fn(&T) -> V, v: (T, U)) -> (V, U) {
+    (f(&v.0), v.1)
+}
+
+// ==================================================================
+// Lists
+// ==================================================================
+
+// forall a . [a] -> a
 pub fn morloc_head<T>(xs: &[T]) -> &T {
     &xs[0]
 }
 
+// forall a . [a] -> [a]
 pub fn morloc_tail<T>(xs: &[T]) -> &[T] {
     &xs[1..]
 }
