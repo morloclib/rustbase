b7551b5e8bb22c430348267525bf061f63740a11
jenra <24357776+jenra-uwu@users.noreply.github.com>
2021-03-03 13:09:42 -0500

    Added higher order functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use std::ops;
 
-// ==================================================================
-// Arithmetic
-// ==================================================================
-
 // id :: forall a . a -> a
 pub fn morloc_id<T>(t: T) -> T{
     t
 }
+
+// ==================================================================
+// Arithmetic
+// ==================================================================
 
 pub fn morloc_add<T, U, V>(t: T, u: U) -> V
     where T: ops::Add<U, Output=V> {
@@ -38,33 +38,47 @@ pub fn morloc_mod(a: i64, b: i64) -> i64 {
 // ==================================================================
 
 // forall a b . (a, b) -> a
-pub fn fst<T, U>(v: &(T, U)) -> &T {
+pub fn morloc_fst<T, U>(v: &(T, U)) -> &T {
     &v.0
 }
 
 // forall a b . (a, b) -> a
-pub fn snd<T, U>(v: &(T, U)) -> &U {
+pub fn morloc_snd<T, U>(v: &(T, U)) -> &U {
     &v.1
 }
 
 // forall a b . a -> b -> (a, b)
-pub fn tuple<T, U>(t: T, u: U) -> (T, U) {
+pub fn morloc_tuple<T, U>(t: T, u: U) -> (T, U) {
     (t, u)
 }
 
 // forall a b . (a -> b) -> a -> (b, a)
-pub fn couple<T, U>(f: fn(&T) -> U, t: T) -> (U, T) {
+pub fn morloc_couple<T, U>(f: fn(&T) -> U, t: T) -> (U, T) {
     (f(&t), t)
 }
 
 // forall a b c . (b -> c) -> (a, b) -> (a, c)
-pub fn with_sec<T, U, V>(f: fn(&U) -> V, v: (T, U)) -> (T, V) {
-    (v.0, f(&v.1))
+pub fn morloc_with_sec<T, U, V>(f: fn(U) -> V, v: (T, U)) -> (T, V) {
+    (v.0, f(v.1))
 }
 
 // forall a b c . (a -> c) -> (a, b) -> (c, b)
-pub fn with_fst<T, U, V>(f: fn(&T) -> V, v: (T, U)) -> (V, U) {
-    (f(&v.0), v.1)
+pub fn morloc_with_fst<T, U, V>(f: fn(T) -> V, v: (T, U)) -> (V, U) {
+    (f(v.0), v.1)
+}
+
+// ==================================================================
+// Higher order functions
+// ==================================================================
+
+// forall a b c . (a -> b -> c) -> [a] -> [b] -> [c]
+pub fn morloc_zip_with<T, U, V>(f: fn(&T, &U) -> V, ts: &[T], us: &[U]) -> Vec<V> {
+    ts.iter().zip(us.iter()).map(|v| f(v.0, v.1)).collect()
+}
+
+// forall a b . (b -> a -> b) -> b -> [a] -> b
+pub fn morloc_fold<T, U>(f: fn(T, &U) -> T, t: T, us: &[U]) -> T {
+    us.iter().fold(t, f)
 }
 
 // ==================================================================
