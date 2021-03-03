3c9173cf41b99863bd1b7969dc98d32c117f18f0
jenra <24357776+jenra-uwu@users.noreply.github.com>
2021-03-03 11:10:57 -0500

    Properly works now

--- lib.rs
+++
@@ -1,3 +0,0 @@
-pub fn morloc_head<T>(xs: Vec<T>) -> &T {
-    return xs[0];
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
+pub fn morloc_head<T>(xs: &[T]) -> &T {
+    &xs[0]
+}
+
+pub fn morloc_tail<T>(xs: &[T]) -> &[T] {
+    &xs[1..]
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_head() {
+        let v = vec![1, 2, 3];
+        assert_eq!(1, *morloc_head(&v));
+    }
+
+    #[test]
+    fn test_tail() {
+        let v = vec![1, 2, 3];
+        assert_eq!([2, 3], *morloc_tail(&v));
     }
 }
+
